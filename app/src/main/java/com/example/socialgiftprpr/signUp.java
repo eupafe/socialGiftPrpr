2127a492a198c29adbee2b8d1c3379006590a15c
Eugènia <eugepach03@gmail.com>
2023-05-17 18:37:37 +0200

    added view

--- app/src/main/java/com/example/socialgiftprpr/signUp.java
+++ app/src/main/java/com/example/socialgiftprpr/signUp.java
@@ -1,19 +1,37 @@
 package com.example.socialgiftprpr;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class signUp extends Fragment {
 
+    private ImageView imageView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        imageView.findViewById(R.id.profileImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);*/
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
+
+
+
 }
