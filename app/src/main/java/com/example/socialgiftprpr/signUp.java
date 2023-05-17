b63dbba8424f10df4e07bb36fc3630da09fe706c
Valèria Ezquerra Rodriguez <valeria.ezquerra@students.salle.url.edu>
2023-05-17 20:12:21 +0200

    Add image profile implemented

--- app/src/main/java/com/example/socialgiftprpr/signUp.java
+++ app/src/main/java/com/example/socialgiftprpr/signUp.java
@@ -1,37 +1,72 @@
 package com.example.socialgiftprpr;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class signUp extends Fragment {
 
     private ImageView imageView;
+    private Uri imagePath;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        imageView.findViewById(R.id.profileImage);
+
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        imageView = view.findViewById(R.id.profileImage);
+
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(intent, 1);
+                startActivityForResult(Intent.createChooser(intent, "Select picture"), 1);
             }
         });
 
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == 1 && resultCode == RESULT_OK && data!=null){
+            imagePath = data.getData();
+            getImageInImageView();
+        }
+    }
+
+    private void getImageInImageView(){
+
+        Bitmap bitmap = null;
+        try{
+            bitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(),imagePath);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+
+    }
 
 }
